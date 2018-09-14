4ab818498e47c02b5278e1004ae046e57c005bde
melanie2016 <mel.lugones@gmail.com>
2018-09-14 01:47:36 -0300

    Mapping y test of Occupation

--- src/main/java/com/cagmeini/serviciosya/dao/orm/OccupationDaoHibernate.java
+++ src/main/java/com/cagmeini/serviciosya/dao/orm/OccupationDaoHibernate.java
@@ -9,23 +9,26 @@ import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
 import com.cagmeini.serviciosya.beans.entity.CityEntity;
+import com.cagmeini.serviciosya.beans.entity.OccupationEntity;
 import com.cagmeini.serviciosya.dao.DaoException;
-import com.cagmeini.serviciosya.dao.interfaces.ICityDao;
+import com.cagmeini.serviciosya.dao.interfaces.IOccupationDao;
 
-public class CityDaoHibernate implements ICityDao {
+public class OccupationDaoHibernate implements IOccupationDao{
 
 	
+	
+
 	private SessionFactory sessionFactory = HibernateUtil.getSessionAnnotationFactory ();
 
 
-    private static final Logger logger= Logger.getLogger (CityDaoHibernate.class);
+    private static final Logger logger= Logger.getLogger (OccupationDaoHibernate.class);
     
 	@Override
-	public void create(CityEntity target) {
+	public void create(OccupationEntity target) {
 
 		if (target == null) {
 
-            logger.warn ("City object is null!");
+            logger.warn ("occupation object is null!");
             return;
         }
 
@@ -37,14 +40,14 @@ public class CityDaoHibernate implements ICityDao {
             session = this.sessionFactory.openSession ();
             tx = session.beginTransaction ();
 
-            logger.debug (String.format ("Creating new city %s", target));
+            logger.debug (String.format ("Creating new occupation %s", target));
             session.save (target);
             tx.commit ();
-            logger.debug (String.format ("New city %s created!", target));
+            logger.debug (String.format ("New occupation %s created!", target));
 
         } catch (Exception e) {
 
-            logger.error (String.format ("Error creating new city %s", target));
+            logger.error (String.format ("Error creating new occupation %s", target));
             tx.rollback ();
             throw new DaoException(e.getMessage (), e);
 
@@ -56,11 +59,11 @@ public class CityDaoHibernate implements ICityDao {
 	}
 
 	@Override
-	public void update(CityEntity target) {
+	public void update(OccupationEntity target) {
 		// Validate the arguments.
 		
 		if(target == null ) {
-			logger.warn ("City object is null");
+			logger.warn ("occupation object is null");
 			return;
 		}
 				
@@ -74,16 +77,16 @@ public class CityDaoHibernate implements ICityDao {
 			session = this.sessionFactory.openSession();
 			t = session.beginTransaction();
 				
-			logger.debug (String.format ("Updating new city %s", target));
+			logger.debug (String.format ("Updating new occupation %s", target));
 					
 			session.update(target);
 			t.commit(); // guardar el cambio 
 					
-			logger.debug(String.format("New city %s" , target));
+			logger.debug(String.format("New occupation %s" , target));
 					
 		}catch (Exception e) {
 					
-			logger.error("Error updating new city");
+			logger.error("Error updating new occupation");
 			t.rollback();
 					
 			throw new DaoException(e.getMessage(), e);
@@ -100,7 +103,7 @@ public class CityDaoHibernate implements ICityDao {
 	public void delete(Integer id) {// Validate the arguments.
 		
 		if(id == null ) {
-			logger.warn ("City object is null");
+			logger.warn ("occupation object is null");
 			return;
 		}
 		
@@ -114,22 +117,22 @@ public class CityDaoHibernate implements ICityDao {
 			session = this.sessionFactory.openSession();
 			t = session.beginTransaction();
 			
-			logger.debug (String.format ("Deleting city by id %s", id.toString ()));
+			logger.debug (String.format ("Deleting occupation by id %s", id.toString ()));
 			// buscamos el objeto por el id q le mando 
 			CityEntity p = (CityEntity) session.get(CityEntity.class, id);
 			
 			if ( p != null) {
 				session.delete(p);
 				t.commit();
-                logger.debug (String.format ("City by id %s deleted!", id.toString ()));
+                logger.debug (String.format ("occupation by id %s deleted!", id.toString ()));
 
 			}else {
-                logger.warn (String.format ("City by id %s not found!", id.toString ()));
+                logger.warn (String.format ("occupation by id %s not found!", id.toString ()));
             }
 			
 		}catch (Exception e) {
 			
-			logger.error (String.format ("Error deleting city id %s", id.toString ()));
+			logger.error (String.format ("Error deleting occupation id %s", id.toString ()));
 			t.rollback();
 			
 			throw new DaoException(e.getMessage(), e);
@@ -142,9 +145,9 @@ public class CityDaoHibernate implements ICityDao {
 
 	
 	@Override
-	public List<CityEntity> findAll() {
+	public List<OccupationEntity> findAll() {
 
-		List<CityEntity> list = null ;
+		List<OccupationEntity> list = null ;
 		
 		Session session = null;
 		try {
@@ -153,11 +156,11 @@ public class CityDaoHibernate implements ICityDao {
             session = this.sessionFactory.openSession ();
 
             logger.debug ("Finding all city");
-            list = (List<CityEntity>) session.createCriteria (CityEntity.class).list ();
+            list = (List<OccupationEntity>) session.createCriteria (OccupationEntity.class).list ();
 
         } catch (Exception e) {
 
-            logger.error ("Error finding all city id");
+            logger.error ("Error finding all occupation id");
             throw new DaoException (e.getMessage (), e);
 
         } finally {
@@ -169,12 +172,12 @@ public class CityDaoHibernate implements ICityDao {
 	}
 
 	@Override
-	public CityEntity findById(Integer id) {
+	public OccupationEntity findById(Integer id) {
 		
 		// Validate the arguments.
         if (id == null) {
 
-            logger.warn ("Id city is null!");
+            logger.warn ("Id occupation is null!");
             return null;
         }
 
@@ -184,8 +187,8 @@ public class CityDaoHibernate implements ICityDao {
             logger.debug ("Getting hibernate session...");
             session = this.sessionFactory.openSession ();
 
-            logger.debug (String.format ("Finding city by id %s", id.toString ()));
-            CityEntity p = (CityEntity) session.createCriteria(CityEntity.class)
+            logger.debug (String.format ("Finding occupation by id %s", id.toString ()));
+            OccupationEntity p = (OccupationEntity) session.createCriteria(CityEntity.class)
             					.add(Restrictions.eq("id",id)).uniqueResult();
             
             		
@@ -194,13 +197,13 @@ public class CityDaoHibernate implements ICityDao {
                 return p;
             } else {
 
-                logger.warn (String.format ("City by id %s not found!", id.toString ()));
+                logger.warn (String.format ("Occupation by id %s not found!", id.toString ()));
                 return null;
             }
 
         } catch (Exception e) {
 
-            logger.error (String.format ("Error finding city id %s", id.toString ()));
+            logger.error (String.format ("Error finding occupation id %s", id.toString ()));
             throw new DaoException (e.getMessage (), e);
 
         } finally {
@@ -211,8 +214,4 @@ public class CityDaoHibernate implements ICityDao {
 		
 	}
 
-	
-	
-	
-	
 }
---
+++ src/test/java/com/cagmeini/serviciosya/dao/OccupationDaoTest.java
@@ -0,0 +1,88 @@
+package com.cagmeini.serviciosya.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.cagmeini.serviciosya.beans.entity.OccupationEntity;
+import com.cagmeini.serviciosya.dao.interfaces.IOccupationDao;
+import com.cagmeini.serviciosya.dao.orm.OccupationDaoHibernate;
+
+public class OccupationDaoTest {
+
+	
+	private IOccupationDao dao = new OccupationDaoHibernate();
+
+    @Test
+    public void testCreate () {
+    	
+    	OccupationEntity parent = new OccupationEntity();
+    	parent.setName("Ocupacion 1");
+    	parent.setDescription("lala..");
+    	parent.setParent(null);
+    	this.dao.create(parent);
+    	
+    	OccupationEntity o1 = new OccupationEntity();
+    	o1.setName("Ocupacion 1");
+    	o1.setDescription("lala..");
+    	o1.setParent(parent);
+    	
+    	this.dao.create(o1);
+    	
+        
+        Assert.assertNotNull ("Failure creating new province.", o1.getId ());
+        
+    }
+
+//    @Test
+//    public void testUpdate () {
+//    	
+//    	CountryEntity c  = new CountryEntity();
+//    	c.setName("Argentina");
+//    	this.daoC.create(c);
+//    	
+//    	ProvinceEntity p = new ProvinceEntity ();
+//        p.setId (11);
+//        p.setName ("BUENOS AIRES");
+//        p.setCountry(c);
+//        this.dao.update (p);
+//
+//        Assert.assertEquals ("Failure updating country.", "BUENOS AIRES", p.getName ());
+//    }
+//
+//    @Test
+//    public void testDelete () {
+//
+//        int id = 10;
+//        this.dao.delete (id);
+//
+//        ProvinceEntity p = this.dao.findById (id);
+//
+//        Assert.assertNull ("Failure deleting country.", p);
+//    }
+//
+//    @Test
+//    public void testFindAll () {
+//
+//        List<ProvinceEntity> list = this.dao.findAll ();
+//
+//        list.forEach (System.out::println);
+//
+//        Assert.assertFalse ("Failure find all countries.", list.isEmpty ());
+//    }
+//    
+//    @Test
+//    public void testFindById () {
+//
+//    	// i know that province name is "Cordoba"
+//    	Integer id = 14;
+//        ProvinceEntity p = this.dao.findById(id);
+//
+//        String valorEsperado = "Cordoba";
+//        
+//        
+////        Assert.assertNotNull(p);
+//        Assert.assertEquals(p.getName(), valorEsperado);
+//        
+//        System.out.println(p.toString());
+//    }
+}
