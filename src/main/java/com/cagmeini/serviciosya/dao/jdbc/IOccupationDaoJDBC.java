9b3dc500db314bc89bb81cf57182ab4680faabe5
melanie2016 <mel.lugones@gmail.com>
2018-09-23 19:01:19 -0300

    cleaning the project

--- src/main/java/com/cagmeini/serviciosya/dao/jdbc/IOccupationDaoJDBC.java
+++
@@ -1,8 +0,0 @@
-package com.cagmeini.serviciosya.dao.jdbc;
-
-import com.cagmeini.serviciosya.beans.domain.Occupation;
-import com.cagmeini.serviciosya.dao.repositories.IDao;
-
-public interface IOccupationDaoJDBC extends IDao<Occupation, Integer> {
-
-}
