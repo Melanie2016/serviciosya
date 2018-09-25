e5296b4140a8a535b5e82c7599ee0c17ab1e7e17
melanie2016 <mel.lugones@gmail.com>
2018-09-24 23:37:09 -0300

    services rest

---
+++ rest-service/src/main/java/com/capgemini/serviciosya/rest/controller/OccupationController.java
@@ -0,0 +1,34 @@
+package com.capgemini.serviciosya.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.cagmeini.serviciosya.dao.repositories.IOccupationRepository;
+
+@RestController
+@RequestMapping ("occupation")
+public class OccupationController {
+	
+	private IOccupationRepository occupationRepository ;
+	
+	
+	public OccupationController() {
+		
+	}
+	
+	@Autowired
+	public void setProductRepository (IOccupationRepository occupationRepository) {
+		
+		this.occupationRepository = occupationRepository;
+	}
+	
+	@RequestMapping (value = "/",method  = RequestMethod.GET)
+	public ResponseEntity<?> get (){
+		
+		return ResponseEntity.ok (this.occupationRepository.findAll ());
+	}
+	
+	
+
+}
