a7b9524cbb68b8310f0c0196114a4485e1c8f0ea
melanie2016 <mel.lugones@gmail.com>
2018-10-01 22:26:00 -0300

    Add findByCountry in provinceController

---
+++ api/src/main/java/com/capgemeini/serviciosya/dao/IProvinceDao.java
@@ -0,0 +1,4 @@
+package com.capgemeini.serviciosya.dao;
+
+public interface IProvinceDao {
+}
---
+++ rest-service/src/main/java/com/capgemini/serviciosya/controller/ProvinceController.java
@@ -0,0 +1,13 @@
+package com.capgemini.serviciosya.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("provinces")
+public class ProvinceController {
+
+
+    
+}
